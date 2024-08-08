290303d62862d4039f2ad567519f590fcb3fdf5f
Behzad Nazari <125150799+nazari1369@users.noreply.github.com>
2024-08-08 21:26:29 +0000

    1

--- hotel_project/hotel_project/settings.py
+++ hotel_project/hotel_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "reception",
+    "restaurant",
     "crispy_forms",
     "crispy_bootstrap5",
 ]
